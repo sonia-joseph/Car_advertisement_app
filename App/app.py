d28028fa90d3a61a35a64e127831d02f8d3e12e4
sonia-joseph <soniamaryjoseph@gmail.com>
2025-03-13 18:39:36 +0100

    logo chnage and app edit

--- App/app.py
+++ App/app.py
@@ -40,7 +40,7 @@ st.dataframe(vdf_clean)
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="crypto.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="vehicle.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(vdf_clean), unsafe_allow_html=True)
